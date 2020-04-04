e9d0f653ec50ace42c6c2cb7b44915cc37bc6ad1
Emanuel Aguero <ema.aguero96@gmail.com>
2020-04-04 11:42:38 -0300

    ejercicio 2 optimizado: heuristica para 6GDL y error de tamaño dentro del IF corregido

--- Codigos Ema/TP1_ej2.py
+++ Codigos Ema/TP1_ej2.py
@@ -1,4 +1,3 @@
-print("Ejercicio3_Robot_6GDL")
 import numpy as np
 from time import time
 import random as rd
@@ -10,9 +9,9 @@ class Nodo:
         self.g = 0
         self.h = 0
         self.f = 0
-    def calculate_h(self,end): #a es un nodo. Heuristica entre nodo current y final
-        self.h = abs(self.pos[0]-end[0])+abs(self.pos[1]-end[1])
-    def calculate_g(self): #camino recorrido
+    def calculate_h(self,end): #Heuristica entre nodo actual y final
+        self.h = abs(self.pos[0]-end[0])+abs(self.pos[1]-end[1])+abs(self.pos[2]-end[2])+abs(self.pos[3]-end[3])+abs(self.pos[4]-end[4])+abs(self.pos[5]-end[5])
+    def calculate_g(self): #Camino recorrido
         self.g = self.g + 1
     def calculate_f(self): 
         self.f = self.g+self.h
@@ -22,18 +21,18 @@ def generate_map(dim):
     return(map)
 
 def generate_obstacles(map,dim):
-    n = int((dim**6)/100) #un decimo del espacio esta ocupado por obstaculos
+    n = int((dim**6)/100) #un centesimo del espacio esta ocupado por obstaculos
     print("Hay " + str(dim**6) + " puntos en el espacio articular" + "\nHay " + str(n) + " obstaculos")
     obstacles = []
     x = dim-1
     for i in range(n):
         a = (rd.randint(0,x),rd.randint(0,x),rd.randint(0,x),rd.randint(0,x),rd.randint(0,x),rd.randint(0,x))
         if map[a[0]][a[1]][a[2]][a[3]][a[4]][a[5]] == 0:
-            #obstacles.append(a)
+            obstacles.append(a)
             map[a[0]][a[1]][a[2]][a[3]][a[4]][a[5]] = 1 #ultimo elemento de la lista obstacles
     return obstacles
 
-def a_star(map,start,end,obstacles):
+def a_star(map,start,end,obstacles,tam): 
     OPEN = []
     CLOSED = []
     start_node = Nodo(None,start)
@@ -65,7 +64,7 @@ def a_star(map,start,end,obstacles):
             pos = (a[0] + current.pos[0],a[1] + current.pos[1],a[2] + current.pos[2],a[3] + current.pos[3],a[4] + current.pos[4],a[5] + current.pos[5])
             if pos in obstacles:
                 continue
-            if (0 <= current.pos[0]+a[0] < 6) and (0 <= current.pos[1]+a[1] < 6) and (0 <= current.pos[2]+a[2] < 6) and (0 <= current.pos[3]+a[3] < 6) and (0 <= current.pos[4]+a[4] < 6) and (0 <= current.pos[5]+a[5] < 6):
+            if (0 <= current.pos[0]+a[0] < tam) and (0 <= current.pos[1]+a[1] < tam) and (0 <= current.pos[2]+a[2] < tam) and (0 <= current.pos[3]+a[3] < tam) and (0 <= current.pos[4]+a[4] < tam) and (0 <= current.pos[5]+a[5] < tam):
                 neighbours.append(Nodo(current,pos))
 
         for neighbour in neighbours:
@@ -78,30 +77,25 @@ def a_star(map,start,end,obstacles):
                 neighbour.padre = current
                 if neighbour not in OPEN and neighbour not in CLOSED:
                     OPEN.append(neighbour)
-            # for open_node in OPEN:
-            #     if neighbour == open_node and neighbour.g > open_node.g:
-            #         continue
-            # OPEN.append(neighbour)
             
 def main():
+    print("Ejercicio3_Robot_6GDL")
     rd.seed(None)
-    t1=time()
-    tam = 5
+    tam = 22 #es la cantidad de puntos articulares por cada GDL
     map = generate_map(tam) 
-    obstacles = [] #generate_obstacles(map,tam)
+    obstacles = generate_obstacles(map,tam)
     #Funcionamiento de randint(a,b): a<=N<=b (nro aleatorio entre a y b, incluidos estos dos)
     start = (rd.randint(0,tam-1),rd.randint(0,tam-1),rd.randint(0,tam-1),rd.randint(0,tam-1),rd.randint(0,tam-1),rd.randint(0,tam-1))
     end = (rd.randint(0,tam-1),rd.randint(0,tam-1),rd.randint(0,tam-1),rd.randint(0,tam-1),rd.randint(0,tam-1),rd.randint(0,tam-1))
 
     print("Inicio: ", start, "\nFin:    ", end)
-    path = a_star(map,start,end,obstacles)
+    t1=time()
+    path = a_star(map,start,end,obstacles,tam) #camino solucion
+    t2 = time()
     print("Camino:")
     for position in path:
         print(position)
-        if position in obstacles:
-            print("es un obtaculo")
 
-    t2 = time()
     print("Tiempo de ejecucion A*: " + str(t2-t1)) 
 
 if __name__ == "__main__":
---
+++ Codigos Ema/TP1_ej6.py
@@ -0,0 +1,17 @@
+#Ejercicio6:
+#Consigna: implementar un AG para resolver el problema de optimizar
+#la ubicacion de los productos del almacen, de manera de optimizar
+#el picking de los mismos. Cosidere que:
+#El layout del almacen esta fijo, solo debe determinarse el lugar 
+#  donde se coloca cada producto
+#Cada producto tiene una ubicacion en el almacen, que define unas
+#  coordenadas, un pasillo y una estanteria
+#Cada orden incluye un conjunto de productos que deben ser 
+#  despachados en su totalidad
+#El pickung comienza y termina en una bahia de carga, de posic fija
+#El "costo" del picking es proporcional a la distancia recorrida
+#Se debe generar un conjunto de ordenes ficticias, simulando ordenes
+#  reales que el almacen tendria que satisfacer. Las ordenes deberian 
+#  tener cantidades distintas de articulos e incluir distinto mix de
+#  articulos (Stock Keeping Unit) cada una.
+
