8ed3b0743f9ada789c9c7ad1bf0a62e548e54d06
Emanuel Aguero <ema.aguero96@gmail.com>
2020-04-08 13:20:15 -0300

    AG almost working

--- Codigos Ema/TP1_ej5.py
+++ Codigos Ema/TP1_ej5.py
@@ -1,58 +1,8 @@
+from TP1_ej3 import generate_map,search_position_of,Nodo
 import numpy as np
 from time import time
 import random as rd
 import math
-
-class Nodo:
-    def __init__(self,padre=None,pos=None):
-        self.padre = padre
-        self.pos = pos
-        self.g = 0
-        self.h = 0
-        self.f = 0
-    def calculate_h(self,end): #a es un nodo. Heuristica entre nodo current y final
-        self.h = abs(self.pos[0]-end[0])+abs(self.pos[1]-end[1])
-    def calculate_g(self): #camino recorrido
-        self.g = self.g + 1
-    def calculate_f(self): 
-        self.f = self.g+self.h
-
-def generate_map(cant_rows_shelves,cant_columns_shelves,n_rows,n_columns):
-    #map generico de un almacen con estantes de 8 espacios
-    x=1
-    row=1
-    column=1
-    pasillo = 3 #nro de column q esta el 1er pasillo
-    m = 1
-    map = np.zeros((n_rows,n_columns),int)
-    while(x<=8*cant_columns_shelves*cant_rows_shelves):
-        map[row][column] = x
-        if x%2==0: #x es par
-            row += 1
-            column -= 1
-        else:
-            column += 1
-        if x%8==0:
-            row+=1
-        x +=1
-        if x==(8*cant_rows_shelves*m)+1:
-            row = 1
-            column = pasillo+1
-            pasillo += 3 
-            m += 1 #para recorrer cada column de shelves
-    print(map)
-    return(map)
-
-def search_position_of(value,map):
-    for index,row in enumerate(map):
-        if value in row: #row es la fila entera. index es el numero de la fila donde esta el value buscado         
-            f = np.ndarray.tolist(row) #row es del tipo numpy.ndarray y no tiene atributo index. con esta linea arreglo eso
-            column = f.index(value)
-            if map[index][column-1] == 0:
-                column -= 1
-            elif map[index][column+1] == 0:
-                column += 1
-            return (index,column)
 
 def simulated_annealing(map,n_rows,n_columns,start,end,T):
     start_node = Nodo(None,start)
@@ -82,18 +32,13 @@ def simulated_annealing(map,n_rows,n_columns,start,end,T):
             current = next
         time+=1
         
-
 def main():
     print("Ejercicio 5:") 
     rd.seed(None)
-    n_rows_shelves = 3     
-    n_columns_shelves = 3  
-    n_rows = 5*n_rows_shelves+1
-    n_columns = 3*n_columns_shelves+1
+    map=generate_map()
     t_prom = 0 #para calcular el promedio de 100 temple simulado
-    map = generate_map(n_rows_shelves,n_columns_shelves,n_rows,n_columns)
 
-    products_list = [3,45,18,27,35,9]
+    products_list = [3,45,18,27,35,9] #debe ser aleatoria
 
     start=search_position_of(min(products_list),map)
     end=search_position_of(max(products_list),map)
@@ -109,7 +54,6 @@ def main():
             if start_node.h<min_h:
                 min_h = start_node.h
                 end = product
-                print(i," product is in ",product, " with H= ",start_node.h)
     end_node = Nodo(None,end)
     print(end_node.pos)
 
@@ -121,7 +65,7 @@ def main():
         t=t*0.9 #func exponencial de T decreciente en el tiempo
     for i in range(20):
         t1 = time()
-        solution_list.append(simulated_annealing(map,n_rows,n_columns,start,end,T))
+        #solution_list.append(simulated_annealing(map,start,end,T))
         t2 = time()
         t_prom = t_prom+t2-t1 #redondeo a 8 decimales    
     #primero hice una lista de soluciones y luego elijo la que
@@ -132,10 +76,7 @@ def main():
         if counter > max_sol:
             max_sol = counter
             solution = sol
-    #"Pintar" solucion con unos
-    print("\nPosicion final: ", solution)
-    map[solution[0]][solution[1]] = 1
-    print("\n",map,"\n\nTiempo de ejecucion promedio de Temple Simulado: ",str(round(t_prom/100,8)))
+    
 
 if __name__ == "__main__":
     main()
--- Codigos Ema/TP1_ej6.py
+++ Codigos Ema/TP1_ej6.py
@@ -8,44 +8,170 @@
 #    coordenadas, un pasillo y una estanteria
 #3) Cada orden incluye un conjunto de productos que deben ser 
 #    despachados en su totalidad
-#4) El pickung comienza y termina en una bahia de carga, de posic fija
+#4) El picking comienza y termina en una bahia de carga, de posic fija
 #5) El "costo" del picking es proporcional a la distancia recorrida
 #6) Se debe generar un conjunto de ordenes ficticias, simulando ordenes
 #    reales que el almacen tendria que satisfacer. Las ordenes deberian 
 #    tener cantidades distintas de articulos e incluir distinto mix de
 #    articulos (Stock Keeping Unit) cada una.
 
-import TP1_ej3 as A_star
+#Individuo: almacen con cierta disposicion de productos
+#Genes: disposicion de productos (48)
+#Fitness: distancia de recocido simulado
+
+from Recocido_Simulado import simulated_annealing, search_position_of, generate_map
 import random as rd
+import numpy as np
+from time import time
 
 class Individuo():
-    def __init__(self,genoma,fitness):
-        self.genoma = genoma
+    def __init__(self,genes=0,fitness=0):
+        self.genes = genes
         self.fitness = fitness
 
-def create_picking_list(map):
+def create_picking_list():
     picking_list = []
-    for i in range(rd.randint(5,10)): #lista de picking variable entre 5 y 10
-        value = rd.randint(1,8*2*3) #valor a buscar aleatorio
-        pos = A_star.search_position_of(value,map)
-        if pos not in picking_list:
-            picking_list.append(pos)
-    print(picking_list) #3
-    return picking_list
+    all_PL = [] #lista de 5 listas picking
+    print("Ordenes ficticias: ")
+    for i in range(5): #5 listas de productos entre 4 y 7 productos c/u 
+        for j in range(rd.randint(4,7)): #lista de picking variable entre 5 y 10
+            value = rd.randint(1,48) #valor a buscar aleatorio
+            if value not in picking_list:
+                picking_list.append(value)
+        all_PL.append(picking_list)
+        print(picking_list)
+        picking_list = []
+    return all_PL #3
+
+def generar_primer_poblacion(n_poblacion):
+    poblacion = [] #poblacion es una lista de objetos Invidividuo
+    #poblacion = np.zeros((n_poblacion,48),int)
+    for i in range(n_poblacion):
+        obj_list = []
+        for j in range(48):
+            producto = rd.randint(1,48) #incluye los extremos 1 y 48
+            while producto in obj_list:
+                producto = rd.randint(1,48)
+            obj_list.append(producto)
+        poblacion.append(Individuo(obj_list)) #pasa de ser una lista a objeto Individuo que contiene esa lista
+    return poblacion
+
+def calcular_fitness(lista,individuo): #individuo ordena nuevamente el mapa en esa disposicion
+    return simulated_annealing(lista,individuo)
+
+def seleccion(poblacion,n_poblacion,all_PL): #selecciono los k mejores.
+    for individuo in poblacion: #para cada indivuduo de la poblacion calcula el fitness como la suma de los temple de todas las listas
+        f_total = 0 #por individuo
+        for lista in all_PL:
+            fitness = calcular_fitness(lista,individuo.genes) 
+            f_total += fitness
+        individuo.fitness = f_total
+        #print(individuo.fitness)
+    k = int(0.25*n_poblacion) #nro de seleccionados
+    seleccionados = []
+    i = 0
+    for m in range(k):
+        seleccionados.append(Individuo())
+    while i!=k:
+        f_min = poblacion[0].fitness #semilla del primer fitness
+        for individuo in poblacion:
+            if individuo.fitness < f_min and individuo not in seleccionados:
+                f_min = individuo.fitness
+                seleccionados[i] = individuo
+        i += 1
+    return seleccionados
+
+def crossover(seleccionados): #cruce de orden
+    for i in range(0,len(seleccionados),2): #los seleccionados pares
+        corte1 = rd.randint(1,46) #el 1 y 46 aseguran q haya al menos un nro a cada lado
+        corte2 = rd.randint(1,46)
+        listaA = seleccionados[i].genes #para tener un nombre +corto
+        listaB = seleccionados[i+1].genes
+        while cruce2 == cruce1: #por si se llegara a repetir
+            cruce2 = rd.randint(1,46)
+        if cruce1 > cruce2: #para mantener que cruce 1 sea menor que cruce2
+            aux = cruce1
+            cruce1 = cruce2
+            cruce2 = aux
+        print("cruce1:",cruce1,"cruce2:",cruce2)
+        print("ListaA:\n",listaA,"ListaB:\n",listaB)
+        newA = []
+        newB = []
+        for m in range(len(listaA)):
+            newA.append(0)
+            newB.append(0)
+        for i in range(corte1,corte2+1):
+            newA[i] = listaB[i]
+            newB[i] = listaA[i]
+
+        it = corte2+1 #sirve para iterar ---LISTA A
+        actual = it #donde se va a guardar
+        while actual != corte1:
+            if actual == len(listaA):
+                actual = 0
+            num = listaA[it]
+            while num in newA:
+                it += 1
+                if it == len(listaA):
+                    it = 0
+                num = listaA[it]
+            newA[actual] = num
+            actual += 1
+
+        it = corte2+1 #sirve para iterar ---LISTA A
+        actual = it #donde se va a guardar
+        while actual != corte1:
+            if actual == len(listaB):
+                actual = 0
+            num = listaB[it]
+            while num in newB:
+                it += 1
+                if it == len(listaB):
+                    it = 0
+                num = listaB[it]
+            newB[actual] = num
+            actual += 1
+        print("ListaA:\n",listaA,"ListaB:\n",listaB)
+        seleccionados[i].genes = listaA
+        seleccionados[i+1].genes = listaB
+    return seleccionados
+
+def evolucion(seleccionados):
+    for i in range(len(seleccionados)):
+        probab_de_mutar = rd.random()
+        if probab_de_mutar < 0.1:
+            gen1 = rd.randint(0,47)
+            gen2 = rd.randint(0,47)
+            while gen1 == gen2: #por si se llegara a repetir
+                gen2 = rd.randint(0,47)
+            #intercambian dos genes
+            aux = seleccionados[i].genes[gen1]
+            seleccionados[i].genes[gen1] = seleccionados[i].genes[gen2]
+            seleccionados[i].genes[gen2] = aux
+    return seleccionados
 
 def genetic_algoritm():
-    individuos = 30
-    max_iter = 50
-    poblacion = []
-    pass
+    all_PL = create_picking_list() #3)
+    n_poblacion = 8 #len(all_PL)) <-- <-- <-- <-- <-- <-- <-- <--
+    poblacion = generar_primer_poblacion(n_poblacion) 
+    
+    generacion = 0
+    max_generacion = 1
+
+    while(generacion<max_generacion):
+        seleccionados = seleccion(poblacion,n_poblacion,all_PL)
+        seleccionados = crossover(seleccionados)
+        seleccionados = evolucion(seleccionados)
+        generacion += 1
 
 def main():
     print("Ejercicio 6")
     rd.seed(None)
-    map = A_star.generate_map(3,3) #1) 2)
-    picking_list = create_picking_list(map) #3)
-    loading_station = (0,3) #4)
-    print(map[picking_list[0]])
+    #loading_station = (0,0) #4)
+    t1 = time()
+    genetic_algoritm()
+    t2 = time()
+    print("Tiempo de ejecucion:",round(t2-t1,2),"segundos")
 
 if __name__ == "__main__":
     main()
