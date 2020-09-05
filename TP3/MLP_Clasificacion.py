b3b7afcd92607ce27e0fe25029c16c266093c832
Agustin Lezcano <lezcanoagustin2012@gmail.com>
2020-09-05 15:30:26 -0300

    graphs added

--- TP3/MLP_Clasificacion.py
+++ TP3/MLP_Clasificacion.py
@@ -25,7 +25,9 @@ def generar_datos_continuos(cantidad_ejemplos, cantidad_clases):
     FACTOR_ANGULO = 0.79
     AMPLITUD_ALEATORIEDAD = 0.1
     n = int(cantidad_ejemplos / cantidad_clases)
-
+#probar
+#    gg=np.random.dirichlet(np.ones(10),size=1)
+######################    
     angulos = np.linspace(1 * np.pi * FACTOR_ANGULO, (1 + 1) * np.pi * FACTOR_ANGULO, n)
     x = np.zeros((cantidad_ejemplos, 1))
     t = np.zeros(cantidad_ejemplos, dtype="uint8") 
@@ -33,9 +35,10 @@ def generar_datos_continuos(cantidad_ejemplos, cantidad_clases):
     z = np.arange(0,cantidad_ejemplos,1)
 
     t = AMPLITUD_ALEATORIEDAD * (np.cos(angulos)+suma)
-    x = t + np.random.uniform(-1,1)
+    x = t + np.random.uniform(-1,1,cantidad_ejemplos)
     
-    plt.plot(z, x, t)
+    #plt.plot(z, x, t)
+    #plt.show()
     
     
     '''
